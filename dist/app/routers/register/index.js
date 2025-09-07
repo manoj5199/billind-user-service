1f9f1efa2d709298a43b216ba637f71e567dd38f
manoj5199 <manojadhi6@gmail.com>
2025-09-07 21:31:58 +0530

    updated

--- dist/app/routers/register/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middleware_1 = require("./middleware");
-const controller_1 = require("./controller");
-// import { db } from "@database/index";
-const router = (0, express_1.Router)();
-router.post("/", middleware_1.validateRegistrationPayload, middleware_1.checkEmailIsAvailable, middleware_1.validatePasswordPolicy, controller_1.register);
-exports.default = router;
